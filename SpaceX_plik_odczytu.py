9cd6e9d46290437ec7b3c76810411dba3c3686be
Niespodzianek <64963974+Niespodzianek@users.noreply.github.com>
2022-02-19 09:22:47 +0100

    jakaś zmiana

--- SpaceX_plik_odczytu.py
+++ SpaceX_plik_odczytu.py
@@ -1,16 +1,18 @@
 import requests
-import json
+# import json
 
 def odczytanie_infornacji(adres_strony):
     strona = requests.get(adres_strony)
     zawartosc = strona.text
-    misje = json.loads(zawartosc)
+    print(zawartosc)
+    # misje = json.loads(zawartosc)
+    # print(misje)
 
-    for index in range(len(misje)):
-        print(f"Pozycja {index + 1}. Nazwa misji: {misje[index]['mission_name']},"
-              f" Symbol: {misje[index]['mission_id']},"
-              f" Inwestor: {misje[index]['manufacturers']},"
-              f" Ładunek: {misje[index]['payload_ids']}")
+    # for index in range(len(misje)):
+    #    print(f"Pozycja {index + 1}. Nazwa misji: {misje[index]['mission_name']},"
+    #          f" Symbol: {misje[index]['mission_id']},"
+    #          f" Inwestor: {misje[index]['manufacturers']},"
+    #          f" Ładunek: {misje[index]['payload_ids']}")
 
         # print(f"Opis: {misje[index]['description']}")
         # for klucz, wartosc in misje[index].items():
